9831bf0a607791a5ad2706527e451ca1d2fc6d86
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-14 13:46:11 +0530

    Added Footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return (<div className="bg-yellow-500 text-center p-3 text-white font-bold"><h3>Made By Shubham Vishwakarma, CF08 </h3></div>)
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from "./components/header/header"
+import Footer from './components/footer/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Header/>{children}</body>
+      <body className={inter.className}><Header/>{children}<Footer/></body>
     </html>
   )
 }
