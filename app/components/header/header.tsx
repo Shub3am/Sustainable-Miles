e9329aefb34bd675fd4887ec6010201195640a5e
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-14 12:49:44 +0530

    Setting Up Home Page

---
+++ app/components/header/header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+export default function Header() {
+    return <header className="text-gray-600 body-font"> <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+      <a className="mr-5 hover:text-gray-900">Home</a>
+      <a className="mr-5 hover:text-gray-900">How It Works</a>
+      <a className="mr-5 hover:text-gray-900">Your Contribution</a>
+      <a className="hover:text-gray-900">Our Idea</a>
+    </nav>
+    <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+      <Image alt="logo" src="/badge.png" width={50} height={50}/>
+      <span className="ml-3 text-xl">Sustainable Miles</span>
+    </a>
+    <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+      <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </button>
+    </div>
+  </div></header>
+}
