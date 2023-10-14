39e79deb5bf61103357c79ed6cbe89658a6a5bbf
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-14 13:21:23 +0530

    Added Benefits section

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
     </div>
     <div className="flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
       <img
-        src="https://cdn.dribbble.com/users/331579/screenshots/3193478/mission_pic_badges_drib.jpg"
+        src="https://cdn.dribbble.com/users/498043/screenshots/4431637/media/1b06654c01c7254bb283f8f886176d43.png"
         className="object-cover object-top w-full h-64 max-w-xl -mb-16 rounded shadow-2xl lg:ml-64 xl:ml-8 lg:-mb-24 xl:-mb-28 lg:h-auto lg:max-w-screen-md"
         alt=""
       />
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Hero from "./components/hero/hero"
 import Features from "./components/features/features"
+import How from "./components/howus/how"
 export default function Home() {
   return (
-    <main><Hero/><Features/></main>
+    <main><Hero/><How/></main>
       )
 }
