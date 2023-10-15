22bb227d07de5da00e6dbee8073889970bb47d7e
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-15 10:38:17 +0530

    Configured Images

--- app/transportList.js
+++ app/transportList.js
@@ -1,28 +1,28 @@
 export const carList = [
     {
-      imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
-      service: 'UberX',
-      multiplier: 1,
+      imgUrl: 'https://cdn.dribbble.com/users/5078530/screenshots/12847567/media/4c770c2fcd630e083bb5767b32c9fdbc.jpg?resize=1000x750&vertical=center',
+      service: 'Govt.of.Delhi Electric Bus',
+      multiplier: 0.15,
     },
     {
-      imgUrl: 'https://i.ibb.co/YDYMKny/uberxl.png',
-      service: 'UberXL',
-      multiplier: 1.5,
+      imgUrl: 'https://cdn.dribbble.com/users/472667/screenshots/5757868/electric-car-icon.png',
+      service: 'Private Electric Car',
+      multiplier: 0.09,
     },
     {
-      imgUrl: 'https://i.ibb.co/Xx4G91m/uberblack.png',
-      service: 'Black',
-      multiplier: 2,
+      imgUrl: 'https://cdn.dribbble.com/users/1004013/screenshots/4694288/media/47a28dd900a659c06b3cda9848f67944.jpg?resize=800x600&vertical=center',
+      service: 'Govt.of.UP CNG BUS',
+      multiplier: 0.05,
     },
     {
-      imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
-      service: 'Comfort',
-      multiplier: 1.2,
+      imgUrl: 'https://cdn.dribbble.com/users/5286903/screenshots/15908376/media/0b1ba45bfcd13bcd452b3f1fdaa40d5b.png?resize=1000x750&vertical=center',
+      service: 'Electric Bike',
+      multiplier: 0.09,
     },
     {
-      imgUrl: ' https://i.ibb.co/1nStPWT/uberblacksuv.png',
-      service: 'Black SUV',
-      multiplier: 2.8,
+      imgUrl: ' https://cdn.dribbble.com/users/472667/screenshots/5757868/electric-car-icon.png',
+      service: 'Private Electric Car',
+      multiplier: 0.12,
     }
   ]
   
