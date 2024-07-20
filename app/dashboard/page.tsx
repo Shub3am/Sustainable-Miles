b4f9f3f0dab2265e3eaeaf51baddffe1d9be19c4
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2024-07-21 00:08:30 +0530

    chore: update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,11 +4,14 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Map from "../components/Map/Map";
+import bookingSvg from "../../public/booking.svg"
+import locationSvg from "../../public/location.svg"
+import ridingSvg from "../../public/location.svg"
 
 export default function Dashboard() {
   const options = [
-    { name: "Ride", href: "/selector", img: "/car.jpg" },
-    { name: "Leaderboard", href: "/leaderboard", img: "/cycle.jpg" },
+    { name: "Ride", href: "/selector", img: ridingSvg },
+    { name: "Leaderboard", href: "/leaderboard", img: locationSvg },
   ];
   const [date, setDate] = useState(new Date().toISOString().slice(0, 16));
   const router = useRouter();
@@ -20,37 +23,40 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       <Map />
-      <div className="flex-1 p-6">
+      <div className="p-6">
         <div className="grid grid-cols-3 gap-4">
           {options.map((opt, id) => (
             <Link href={opt.href} key={id} passHref>
-              <div className="flex flex-col flex-1 m-1 h-52 items-center justify-center rounded-lg shadow-md transform hover:scale-105 transition text-xl cursor-pointer relative overflow-hidden ">
+              <div className="flex flex-col bg-white flex-1 m-1 h-64 items-center justify-end rounded-lg shadow-lg transform text-xl cursor-pointer px-5 py-12">
+                <div className="w-44 h-44">
                 <Image
-                  className="absolute inset-0 w-full h-full object-cover"
                   src={opt.img}
                   alt={opt.name}
-                  layout="fill"
                 />
-                <p className="relative z-10 text-white p-2 text-5xl bg-black bg-opacity-70">
+                </div>
+                <p className="text-white p-2 text-lg lg:w-2/3 rounded-md text-center mt-6 bg-theme-primary">
                   {opt.name}
                 </p>
               </div>
             </Link>
           ))}
-          <div className="flex flex-col bg-white flex-1 m-1 h-52 items-center justify-center rounded-lg shadow-md transform text-xl cursor-pointer text-green-600">
+          <div className="flex flex-col bg-white flex-1 m-1 h-64 items-center justify-center rounded-lg shadow-lg transform text-xl cursor-pointer px-5 py-12">
+            <div className="w-44 h-44">
+              <Image src={bookingSvg} alt="booking" className="" layout="" />
+            </div>
             <input
               id="date"
               type="datetime-local"
               name="date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
-              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="lg:w-2/3 mt-6 px-4 py-5 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-theme-primary text-white"
             />
           </div>
         </div>
 
         <div className="flex justify-center mt-4">
-          <Link href="/selector" passHref>
+          <Link href="/search" passHref>
             <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
               Where to?
             </button>
